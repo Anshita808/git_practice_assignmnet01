0463fd954e716b6b48aecd533b94e105a2422a41
Anshita <anshitagupta783@gmail.com>
2022-11-02 13:40:07 +0530

    fixed the code

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 let num=19;
 let factor=0;
 for(let i=1; i<=num; i++){
-    if(i%num){
+    if(i%num==0){
     factor++;
     }
-}if(factor=2){
+}if(factor==2){
     console.log("Prime");
 }else{
     console.log("Not a Prime");
